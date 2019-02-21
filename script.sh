770d32aaa138ea85cd5ec933b5f7a2bedd468747
Soumik <soumik@soumikghosh.com>
2019-02-21 13:55:16 -0500

    Specify region

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+aws configure --region us-east-2
 export SOURCE_FILE="s3://vtlib-imgstore/Ms1996_008_CarpenterHenry_Complete/Ms1996_008_1863_0904a.tif"
 export DEST_FILE="s3://vtlib-imgdeposit/Ms1996_008_1863_0904a.png"
 aws s3 cp "${SOURCE_FILE}" ./tmp.tif || error_exit "Failed to download source file from ${SOURCE_FILE}"

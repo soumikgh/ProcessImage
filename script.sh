eb3880ee008a0f7afe871fc835a18224aa8650a0
Soumik <soumik@soumikghosh.com>
2019-02-21 12:45:14 -0500

    Add processing script

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export SOURCE_FILE="s3://vtlib-imgstore/Ms1996_008_CarpenterHenry_Complete/Ms1996_008_1863_0904a.tif"
+export DEST_FILE="s3://vtlib-imgdeposit/Ms1996_008_1863_0904a.png"
+aws s3 cp "${SOURCE_FILE}" ./tmp.tif || error_exit "Failed to download source file from ${SOURCE_FILE}"
+convert tmp.tif -resize 50% tmp.png
+aws s3 cp tmp-0.png "${DEST_FILE}"

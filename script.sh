5770a8820b28e224915279d19b0381480b9eee0c
Soumik <soumik@soumikghosh.com>
2019-02-21 16:59:57 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ export SOURCE_FILE="s3://vtlib-imgstore/Ms1996_008_CarpenterHenry_Complete/Ms199
 export DEST_FILE="s3://vtlib-imgdeposit/Ms1996_008_1863_0904a.png"
 aws s3 cp "${SOURCE_FILE}" ./tmp.tif || error_exit "Failed to download source file from ${SOURCE_FILE}"
 convert tmp.tif -resize 50% tmp.png
-aws s3 cp tmp-0.png "${DEST_FILE}"
+aws s3 cp tmp-0.png "${DEST_FILE}" || error_exit "Failed to upload processed image to ${DEST_FILE}"

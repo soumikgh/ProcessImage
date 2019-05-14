c45d094bc987c7df508408929510c69013dad4ce
Soumik <soumik@soumikghosh.com>
2019-05-14 11:57:54 -0400

    Use cloned script

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export SOURCE_FILE="s3://${SRC_BUCKET}/${SRC_KEY}"
-export DEST_FILE="s3://${DEST_BUCKET}/${DEST_KEY}"
-aws s3 cp "${SOURCE_FILE}" ./tmp.tif || error_exit "Failed to download source file from ${SOURCE_FILE}"
-convert tmp.tif -resize 50% tmp.png
-aws s3 cp tmp-0.png "${DEST_FILE}" || error_exit "Failed to upload processed image to ${DEST_FILE}"
+cd /opt
+git clone https://github.com/VTUL/iiif_s3_script.git
+cd iiif_s3_script
+chmod +x main.sh
+./main.sh
